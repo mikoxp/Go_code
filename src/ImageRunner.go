24961e52a06946a5ea5925a87bc8ef718b64844d
olesmichalxp@tlen.pl <olesmichalxp@tlen.pl>
2020-11-09 21:56:39 +0100

    Progressing white and black & sepia

--- src/ImageRunner.go
+++ src/ImageRunner.go
@@ -1,27 +1,34 @@
 package main
 
 import (
-	"image"
 	progressing "Go_code/src/image_progressing"
 )
 
 func main() {
 	path := "image/lena.jpg"
-	pathOUT:="image/image.jpg"
+	pathBW:="image/lena_black_white.jpg"
+	pathSepia20:="image/lena_sepia20.jpg"
+	pathSepia30:="image/lena_sepia30.jpg"
+	pathSepia40:="image/lena_sepia40.jpg"
 	rgba := progressing.OpenRGBAImage(path)
 
-	width := rgba.Rect.Dx()
-	height := rgba.Rect.Dy()
-	upLeft := image.Point{0, 0}
-	lowRight := image.Point{width, height}
-	result := image.NewRGBA(image.Rectangle{upLeft, lowRight})
+	//width := rgba.Rect.Dx()
+	//height := rgba.Rect.Dy()
+	//upLeft := image.Point{0, 0}
+	//lowRight := image.Point{width, height}
+	//result := image.NewRGBA(image.Rectangle{upLeft, lowRight})
 	//cyan := color.RGBA{100, 200, 200, 0xff}
-	for x := 0; x < width; x++ {
-		for y := 0; y < height; y++ {
-			p := rgba.RGBAAt(x, y)
-			result.Set(x, y, p)
-		}
-	}
-	progressing.SaveImage(pathOUT, result,"JPG")
+
+	result:=progressing.TransformationBlackAndWhite(rgba)
+	progressing.SaveImage(pathBW, result,"JPG")
+
+	result=progressing.TransformationSepia(rgba,20)
+	progressing.SaveImage(pathSepia20, result,"JPG")
+
+	result=progressing.TransformationSepia(rgba,30)
+	progressing.SaveImage(pathSepia30, result,"JPG")
+
+	result=progressing.TransformationSepia(rgba,40)
+	progressing.SaveImage(pathSepia40, result,"JPG")
 
 }
--- src/image_progressing/ImageProgressing.go
+++ src/image_progressing/ImageProgressing.go
@@ -6,10 +6,8 @@ import (
 	"image/jpeg"
 	"image/draw"
 	"image/png"
+	"image/color"
 )
-type Figure interface {
-	field() float64
-}
 func OpenRGBAImage(path string) *image.RGBA {
 	imgfile, err := os.Open(path)
 	if err != nil {
@@ -36,4 +34,47 @@ func SaveImage(path string, img *image.RGBA,fileType string)  {
 	case "JPG":
 		jpeg.Encode(f,img, &jpeg.Options{jpeg.DefaultQuality})
 	}
+}
+
+func TransformationBlackAndWhite(img *image.RGBA) *image.RGBA{
+	width := img.Rect.Dx()
+	height := img.Rect.Dy()
+	result:=createEmptyImage(width,height)
+	for x := 0; x < width; x++ {
+		for y := 0; y < height; y++ {
+			p := img.RGBAAt(x, y)
+			val:=(int32(p.R)+int32(p.G)+int32(p.B))/3
+			c:=uint8(val)
+			result.Set(x, y, color.RGBA{R:c,G:c,B:c,A:p.A})
+		}
+	}
+	return result
+}
+
+func TransformationSepia(img *image.RGBA,w int32) *image.RGBA{
+	width := img.Rect.Dx()
+	height := img.Rect.Dy()
+	result:=createEmptyImage(width,height)
+	for x := 0; x < width; x++ {
+		for y := 0; y < height; y++ {
+			p := img.RGBAAt(x, y)
+			r:=int32(p.R)*2+w
+			if(r>255){
+				r=255
+			}
+			g:=int32(p.G)+w
+			if(g>255){
+				g=255
+			}
+			result.Set(x, y, color.RGBA{R:uint8(r),G:uint8(g),B:p.B,A:p.A})
+		}
+	}
+	return result
+}
+
+func createEmptyImage(width int,height int) *image.RGBA{
+	upLeft := image.Point{0, 0}
+	lowRight := image.Point{width, height}
+	result := image.NewRGBA(image.Rectangle{upLeft, lowRight})
+	return result
 }
